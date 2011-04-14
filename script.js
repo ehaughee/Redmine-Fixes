6061929c4a8ac99198018c59a2c028cde7c9e409
Eric <eric@Eric-Ubuntu.(none)>
2011-04-14 13:21:02 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var timeEntries = "time_entries";
+var url = document.URL;
+url = url.substring(0, 36);
+
+if (document.URL[document.URL.length - 1] != '/')
+	timeEntries = "/time_entries";
+
+if (url == 'http://support.utiliflex.com/issues/')
+	if (document.URL.indexOf("time_entries") == -1)
+		document.getElementById("content").children[0].innerHTML = "<a href='" + document.URL + timeEntries + "'>Time Entries</a>" + document.getElementById("content").children[0].innerHTML;

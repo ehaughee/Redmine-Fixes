626b6ae69e9b62540895d3ee8f2cca5985458062
Eric Haughee <ehaughee@gmail.com>
2011-04-20 12:25:32 -0400

    better div targeting for anchor insertion

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ url = url.substring(0, 36);
 if (document.URL[document.URL.length - 1] != '/')
 	timeEntries = "/time_entries";
 
-if (url == 'http://support.utiliflex.com/issues/')
-	if (document.URL.indexOf("time_entries") == -1)
-		if (document.getElementById("content").children[0].className.contains("contextual") == -1)
-			document.getElementById("content").children[0].innerHTML = "<a href='" + document.URL + timeEntries + "'>Time Entries</a>" + document.getElementById("content").children[1].innerHTML;
-		else
-			document.getElementById("content").children[0].innerHTML = "<a href='" + document.URL + timeEntries + "'>Time Entries</a>" + document.getElementById("content").children[0].innerHTML;
+var context = document.getElementById("watcher").parentNode
+var time_entries_anchor = "<a href='" + document.URL + timeEntries + "'>Time Entries</a>";
+if (document.URL.indexOf("time_entries") == -1)
+	context.innerHTML = time_entries_anchor + context.innerHTML;
